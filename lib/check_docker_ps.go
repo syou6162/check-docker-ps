d518541d758da355278a1066bc546151debb2c07
Yasuhisa Yoshida <syou6162@gmail.com>
2017-11-12 01:46:27 +0900

    process存在確認するように

--- lib/check_docker_ps.go
+++ lib/check_docker_ps.go
@@ -1,8 +1,12 @@
 package checkdockerps
 
 import (
-	"os"
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/mackerelio/checkers"
+	"os"
 )
 
 // Do the plugin
@@ -13,7 +17,27 @@ func Do() {
 }
 
 func run(args []string) *checkers.Checker {
-	checkSt := checkers.OK
+	if len(args) != 1 {
+		return checkers.NewChecker(checkers.UNKNOWN, "No container name specified")
+	}
+	containerName := args[0]
 
-	return checkers.NewChecker(checkSt, "Process exists")
+	ctx := context.Background()
+	client, err := client.NewEnvClient()
+	if err != nil {
+		return checkers.NewChecker(checkers.UNKNOWN, err.Error())
+	}
+
+	containers, err := client.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		return checkers.NewChecker(checkers.UNKNOWN, err.Error())
+	}
+
+	checkSt := checkers.CRITICAL
+	for _, container := range containers {
+		if container.Names[0] == "/"+containerName {
+			return checkers.NewChecker(checkers.OK, fmt.Sprintf("Process %s exists", containerName))
+		}
+	}
+	return checkers.NewChecker(checkSt, fmt.Sprintf("Process %s does not exists", containerName))
 }
